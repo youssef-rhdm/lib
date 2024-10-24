4053f122fd8c30f29499a5ab9a446e6ddd4b31bf
youssef <youssef.rhandoumi@gmail.com>
2024-10-24 10:01:57 +0100

    hi

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhandou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 11:18:33 by yrhandou          #+#    #+#             */
+/*   Updated: 2024/10/23 14:41:58 by yrhandou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "./ft_strlen.c"
+#include "./ft_isdigit.c"
+#include "./ft_isprint.c"
+#include "./ft_isascii.c"
+#include "./ft_isalpha.c"
+#include "./ft_isalnum.c"
+#include "./ft_toupper.c"
+#include "./ft_tolower.c"
+#include "./ft_strchr.c"
+
+int main()
+{
+	// int i = 32;
+	// while ( i<= 127)
+	// {
+	// 	printf("the character %c(%d) is====> %c \n", i, i, tolower(i));
+	// 	i++;
+	// }
+	printf("result : %s\n", ft_strchr("lorem ipsum fasum",'m'));
+	printf("result : %s\n", strrchr("lorem ipsum fasum",'m'));
+	return 0;
+}
