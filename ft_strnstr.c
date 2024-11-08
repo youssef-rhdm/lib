e9e6ec91e55ee2ab8d28ebb08baa6fb16a392260
Youssef Rhandoumi <yrhandou@e3r10p9.1337.ma>
2024-11-08 18:07:07 +0100

    test strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:41:54 by yrhandou          #+#    #+#             */
-/*   Updated: 2024/11/08 16:59:37 by yrhandou         ###   ########.fr       */
+/*   Updated: 2024/11/08 18:03:04 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		{
 			j = 0;
 			k = i;
-			while (needle[j] && k < len)
-			{
-				if (haystack[k] == needle[j])
+			while (needle[j] && k < len && haystack[k] == needle[j])
 				{
 					k++;
 					j++;
 				}
 				if (needle[j] == '\0')
 					return ((char *)(&haystack[i]));
-			}
 		}
 		i++;
 	}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:18:33 by yrhandou          #+#    #+#             */
-/*   Updated: 2024/11/08 16:58:42 by yrhandou         ###   ########.fr       */
+/*   Updated: 2024/11/08 17:57:35 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main()
 	char *str1 = "aslkmask";
 	char  tt[] = "9223372036854775809";
 		printf("\e[0;31m");
-		printf("Red : %s\n", ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15));
+		printf("Red : %s\n", ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30));
 		printf("\e[0;32m");
 		printf("Green :%s\n", strnstr("lorem ipsum dolor sit amet", "dolor", 15));
 
