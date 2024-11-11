92de254fae7500c64fcb9e2bcbbeb1f5914434ee
Youssef Rhandoumi <yrhandou@e2r5p8.1337.ma>
2024-11-11 11:44:35 +0100

    Added ft_itoi and updatd git ignore

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,61 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 08:46:40 by yrhandou          #+#    #+#             */
-/*   Updated: 2024/11/09 10:56:57 by yrhandou         ###   ########.fr       */
+/*   Updated: 2024/11/11 11:42:15 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static int	count_num(int num)
+{
+	int	count;
+
+	count = 1;
+	while (num / 10 != 0)
+	{
+		count++;
+		num = num / 10;
+	}
+	return count;
+}
+
+static char *itoa_ft(int count , int n)
+{
+	char	*str;
+
+	str = (char *)malloc(count + 2);
+	if (!str)
+		return (NULL);
+	str[count+1] = '\0';
+	str[0] = '-';
+	while (count)
+	{
+		str[count--] = (n % 10)*(-1)  + '0';
+		n = n / 10;
+	}
+	return (str);
+}
 
 char	*ft_itoa(int n)
 {
-	return;
+	int		count;
+	char	*str;
+
+	count = count_num(n);
+	printf("%d\n", count);
+	if (n<0)
+		return itoa_ft(count,n);
+	str = (char *)malloc(count + 2);
+	if (!str)
+		return (NULL);
+	else
+		str[count] = '\0';
+	while(count--)
+	{
+		str[count] = n%10 +'0';
+		n = n/10;
+	}
+	return (str);
 }
