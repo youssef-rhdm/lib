6b7a3a82a8c4715a6b95a9ebd9542bdb2f839184
Youssef Rhandoumi <yrhandou@e2r5p8.1337.ma>
2024-11-11 14:31:11 +0100

    Created A working prototype of split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,34 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 11:49:47 by yrhandou          #+#    #+#             */
-/*   Updated: 2024/11/09 10:55:25 by yrhandou         ###   ########.fr       */
+/*   Updated: 2024/11/11 13:35:24 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int delimiter_count(char *str,char c)
+{
+	int i;
+
+	i =0;
+	while (str[i] == c)
+		i++;
+	return (i);
+
+}
+
+
 char	**ft_split(char const *s, char c)
 {
-	return;
+	int	i;
+	int	x;
+	int	y;
+	char **str;
+	
+	str = (char **)malloc(sizeof(char) * ft_strlen(s)+1);
+	if(!str)
+		return (NULL);
+
+	return (str);
 }
---
+++ main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 11:18:33 by yrhandou          #+#    #+#             */
+/*   Updated: 2024/11/11 14:29:12 by yrhandou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	int i = 0, j, x = 0, y = 0;
+	char *src = "EU_LA_UK";
+
+	char str[10][10];
+	char c = '_';
+	while (src[i] != '\0')
+	{
+		if (src[i] == c)
+		{
+			j = 0;
+			while (y < i)
+			{
+				str[x][j] = src[y];
+				y++;
+				j++;
+			}
+			str[x][j] = '\0';
+			x++;
+			y++;
+		}
+		i++;
+		if (src[i] == '\0')
+		{
+			j = 0;
+			while (y < i)
+			{
+				str[x][j] = src[y];
+				y++;
+				j++;
+			}
+			str[x][j] = '\0';
+		}
+
+	}
+	printf("%s, %s, %s", str[0], str[1], str[2]);
+	return 0;
+}
